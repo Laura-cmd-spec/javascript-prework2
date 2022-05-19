ff045711f718d6643fd15df9b6905548321d13df
Laura Krupa <laura.krupa@astellas.com>
2022-05-20 00:20:23 +0200

    Init commit

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let computerMove = `rock`;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerMove = `paper`;
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
