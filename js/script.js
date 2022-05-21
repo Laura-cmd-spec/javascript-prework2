488888b1dc2e7fd77fdc2f26ded2144457f1f7a4
Laura Krupa <laura.krupa@astellas.com>
2022-05-21 21:56:47 +0200

    add second element of game

--- js/script.js
+++ js/script.js
@@ -3,37 +3,44 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
-
-if(randomNumber == 1){
-  computerMove = 'rock';
-
-printMessage('Mój ruch to: ' + computerMove);
+if(randomNumber =='1'){
+  computerMove ='rock';
+}else if (randomNumber =='2'){
+    computerMove ='paper';
+}else {(randomNumber =='3')
+    computerMove ='scissors';
 }
-else if (randomNumber == 2){
-    computerMove='paper';
+printMessage('My move: ' + computerMove);    
 
-printMessage('Mój ruch to:' + computerMove);
+let playerInput = prompt('Choose your move! 1: rock, 2: paper, 3: scissors.');
+
+console.log('Gracz wpisał:' + playerInput);
+let playerMove ='nieznany ruch';
+if(playerInput =='1'){
+  playerMove ='rock';
+}else if (playerInput == '2'){
+    playerMove ='paper';
+}else {(playerInput == '3')
+    playerMove ='scissore';
 }
-else  {(randomNumber == 3)
-    computerMove='scissors'
-printMessage('Mój ruch to:' + computerMove);
-}
+printMessage('Your move: ' + playerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
-if(playerInput == '1'){
-  playerMove = 'rock';
-printMessage('Twój ruch to: ' + playerMove);
-}
-else if (playerInput == '2'){
-    playerMove = 'paper';
-printMessage('Twój ruch to: ' + playerMove);
-}
-else {(playerInput == '1')
-    playerMove = 'scissore';
-printMessage('Twój ruch to: ' + playerMove);
+if(playerMove =='Nieznany ruch') {
+    printMessage('Error');
+}else if( computerMove == 'rock' && playerMove == 'paper'){
+    printMessage('You Won!');
+}else if( computerMove == 'paper' && playerMove == 'scissors'){
+    printMessage('You Won!');
+}else if( computerMove == 'scissors' && playerMove == 'rock'){
+    printMessage('You won!');
+}else if( computerMove == 'rock' && playerMove == 'scissors'){
+    printMessage('You Won!');
+}else if( computerMove == 'paper' && playerMove == 'rock'){
+    printMessage('You lost!');
+}else if( computerMove == 'paper' && playerMove == 'rock'){
+    printMessage('You lost!');
+}else if( computerMove == 'scissors' && playerMove == 'paper'){
+    printMessage('You lost!');
+}else {('computerMove == playerMove')
+    printMessage('Draw!');
 }
