701f8f3699c95fccb6ae8d6832a1df2486914944
Laura Krupa <laura.krupa@astellas.com>
2022-05-21 19:32:07 +0200

    add second element of game

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
-let computerMove = `rock`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-let playerMove = `paper`;
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'rock';
+
+printMessage('Mój ruch to: ' + computerMove);
+}
+else if (randomNumber == 2){
+    computerMove='paper';
+
+printMessage('Mój ruch to:' + computerMove);
+}
+else  {(randomNumber == 3)
+    computerMove='scissors'
+printMessage('Mój ruch to:' + computerMove);
+}
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'rock';
+printMessage('Twój ruch to: ' + playerMove);
+}
+else if (playerInput == '2'){
+    playerMove = 'paper';
+printMessage('Twój ruch to: ' + playerMove);
+}
+else {(playerInput == '1')
+    playerMove = 'scissore';
+printMessage('Twój ruch to: ' + playerMove);
+}
